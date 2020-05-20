9a5fc4e634417b8a78358adf5e05eba7c68d2bbb
Haeyoon Chang <haeyoon@pdx.edu>
2020-05-20 09:51:06 -0700

    section to insert daily transaction details

---
+++ cooldash/src/components/record-transaction.component.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class DataInput extends Component {
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.handleTicker = this.handleTicker.bind(this);
+        this.handleOrder = this.handleOrder.bind(this);
+        this.handleNumShare = this.handleNumShare.bind(this);
+        this.handleSharePrice = this.handleSharePrice.bind(this);
+        this.handlePrediction = this.handlePrediction.bind(this);
+        this.handleActual = this.handleActual.bind(this);
+
+        this.state = {
+            ticker: '',
+            order: '', 
+            num_share: '',
+            per_share_price: '',
+            prediction: '',
+            actual: ''
+        }
+    }
+
+    handleTicker(e) { 
+        this.setState({
+            ticker: e.target.value
+        });
+    }
+
+    handleOrder(e) { 
+        this.setState({
+            order: e.target.value
+        });
+    }
+ 
+    handleNumShare(e) { 
+        this.setState({
+            num_share: e.target.value
+        });
+    }
+
+    handleSharePrice(e) { 
+        this.setState({
+            per_share_price: e.target.value
+        });
+    }
+ 
+    handlePrediction(e) { 
+        this.setState({
+            prediction: e.target.value
+        });
+    }
+ 
+    handleActual(e) {
+        this.setState({
+            actual: e.target.value
+        });
+    }
+
+    onSubmit(e) { 
+        e.preventDefault(); 
+
+        console.log("will it work?"); 
+        const dailyrecord = {
+            ticker: this.state.ticker,
+            order: this.state.order, 
+            num_share: this.state.num_share,
+            per_share_price: this.state.per_share_price,
+            prediction: this.state.prediction,
+            actual: this.state.actual
+        }
+
+        console.log(dailyrecord);
+        console.log("success");
+
+        axios.post('/admin/add', dailyrecord)
+            .then(res => {
+                console.log(res.data);
+            });
+        
+        window.location = '/';
+    }
+
+	render() {
+		return (
+            <form style={{ margin: 10 }} onSubmit={this.onSubmit}>
+				<h1> Daily Trading Log </h1>
+				<section>
+					<label htmlFor="ticker">Ticker</label>
+                    <input type="text" id="ticker" onChange={this.handleTicker}/>
+				</section>
+				<section>
+					<label htmlFor="order">Order</label>
+                    <select name="" id="order" onChange={this.handleOrder}>
+						<option value="sell">Sell</option>
+						<option value="buy">Buy</option>
+						<option value="hold">Hold</option>
+					</select>
+				</section>
+				<section>
+					<label htmlFor="num-share">Number of Shares</label>
+                    <input type="number" id="num-share" onChange={this.handleNumShare} />
+				</section>				<section>
+					<label htmlFor="per-share-price">Per Share Price</label>
+                    <input type="number" step=".01" id="per-share-price" onChange={this.handleSharePrice}/>
+				</section>		
+				<section>
+					<label htmlFor="prediction">Prediction</label>
+                    <input type="number" step=".01" id="prediction" onChange={this.handlePrediction}/>
+				</section>
+				<section>
+					<label htmlFor="actual">Actual Performance</label>
+                    <input type="number" step=".01" id="actual" onChange={this.handleActual}/>
+				</section>
+				<section className="button-container">
+					<button className="button" type="submit">Submit</button>
+					<button className="button" type="reset">Reset</button>
+				</section>
+			</form>
+		);
+	}
+}
+
+export default DataInput; 
