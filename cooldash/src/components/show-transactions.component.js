d4803af06454a65215d7a848ef4ccde1c888514f
Haeyoon Chang <haeyoon@pdx.edu>
2020-05-20 09:51:19 -0700

    section to show past transactions table

---
+++ cooldash/src/components/show-transactions.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+
+class ShowTransactions extends Component { 
+    render() { 
+        return (
+            <div>This section shows transactions history</div>
+        );
+    }
+}
+
+export default ShowTransactions; 
