1417266dc640b9110ca71859441b48385c87d04e
Haeyoon Chang <haeyoon@pdx.edu>
2020-05-31 12:18:07 -0700

    quote api added to main component

--- cooldash/src/components/maindash.component.js
+++ cooldash/src/components/maindash.component.js
@@ -1,15 +1,28 @@
 import React, { PureComponent } from 'react';
 import Newsapp from './Newsapp.js';
 import Pricegraph from './Pricegraph.js';
+import SectorPerformance from './SectorPerformance.js';
 
 function SplitPane(props) {
 	return (
 	  <div className="SplitPane">
-			<span id = "leftplane" class = "Left">
-			{props.left}
+			<span id = "leftplane" className = "Left">
+				{props.left}
 			</span>
-			<span id = "rightplane" class = "Right">
-			{props.right}
+			<span id = "rightplane" className = "Right">
+				{props.right}
+			</span>
+			<span id = "middleleftplane" className = "MiddleLeft">
+				{props.middleleft}
+			</span>
+			<span id = "middlerightplane" className = "MiddleRight">
+				{props.middleright}
+			</span>
+			<span id="bottomleftplane" className = "BottomLeft">
+				{props.bottomleft}
+			</span>
+			<span id="bottomrightplane" className = "BottomRight">
+				{props.bottomright}
 			</span>
 	  </div>
 	);
@@ -18,43 +31,63 @@ function SplitPane(props) {
 export default class MainDashboard extends PureComponent {
 	constructor(props) {
 		super(props);
-		this.state = {value: props.ticker};
+		this.state = {
+			value: props.ticker,
+			quote: "", 
+			author: "", 
+		};
 		this.handleClick = this.handleClick.bind(this);
-		// this.goToRecordTransaction = this.goToRecordTransaction.bind(this);
 	}
 	
 	handleClick() {
 		let newTicker = document.getElementById("ticker_text").value;
-		this.setState({value: newTicker}, function () {
-			
-		});
+		this.setState({value: newTicker});
 	}
-
-	// goToRecordTransaction() { 
-	// 	window.location = "/admin";
-	// }
 
 	render() {
 
 		return (
 			<div><h1 class = "titlebar">Welcome to Luke and Haeyoon's Trading Dashboard!</h1>	
-			<div class = "outterbox">
-				<p>Ticker: <input type="text" id="ticker_text" name="ticker_text"></input> 
-				<button onClick={this.handleClick}>
-        			Search
-      			</button> </p>
-				<SplitPane
-					left={
-						<Newsapp id="newsapp" ticker={this.state.value} />
-					}
-					right= {
-						<Pricegraph id="pricegraph" ticker={this.state.value} />
-					}
-				/>
-				<br />
-				{/* <button onClick={this.goToRecordTransaction}>Record Transaction</button> */}
-      		</div>
-			</div	>
+				<div class="outterbox">
+					<p id="quote">{this.state.quote}</p>
+					<p id="author">{this.state.author}</p>
+					<p>Ticker: <input type="text" id="ticker_text" name="ticker_text"></input> 
+					<button onClick={this.handleClick}>
+						Search
+					</button> </p>
+					<SplitPane
+						left={<Newsapp id="newsapp" ticker={this.state.value} news={this.state.news}/>}
+						middleleft={<Pricegraph id="pricegraph" ticker={this.state.value} />}
+						middleright={<SectorPerformance />}
+					/>
+				</div>
+			</div>
 		);
+	}
+
+	getQuote() { 
+		fetch("https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info", {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com",
+				"x-rapidapi-key": process.env.REACT_APP_RAPID_KEY,
+			}
+		})
+		.then(response => response.json())
+		.then(response => {
+			console.log(response);
+			this.setState({
+				quote: response.text,
+				author: response.author,
+			});
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+
+	componentDidMount() { 
+		this.getQuote(); 
+		this.render(); 
 	}
 }
