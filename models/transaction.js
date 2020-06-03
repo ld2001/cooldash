7b2f95021289d69c70415f4462373ca5da76e093
Haeyoon Chang <haeyoon@pdx.edu>
2020-06-03 16:38:09 -0700

    delete unnecessary lines

--- models/transaction.js
+++ models/transaction.js
@@ -5,40 +5,12 @@ var transactions = {
       AttributeName: 'trans_id',
       AttributeType: 'S'
     }
-      // {
-      //   AttributeName: 'ticker',
-      //   AttributeType: 'S'
-      // },     
-      // {
-      //     AttributeName: 'date',
-      //     AttributeType: 'S'
-      // }, 
-    //   {
-    //     AttributeName: 'order',
-    //     AttributeType: 'S'
-    //   },
-    //   {
-    //       AttributeName: 'num_share',
-    //       AttributeType: 'N'
-    //   },
-    //   {
-    //       AttributeName: 'per_share_price',
-    //       AttributeType: 'N'
-    //   }
     ],
   KeySchema: [
     {
       AttributeName: 'trans_id',
       KeyType: 'HASH'
     }
-      // {
-      //   AttributeName: 'ticker',
-      //   KeyType: 'HASH'
-      // }, 
-      // {
-      //   AttributeName: 'date',
-      //   KeyType: 'RANGE'
-      // },
     ],
     ProvisionedThroughput: {
       ReadCapacityUnits: 1,
